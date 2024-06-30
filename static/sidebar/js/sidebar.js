6b08a03a1243f85b1be8e062d2e99ce5b836cd68
paolovg95 <paolo9517@gmail.com>
2024-06-30 00:29:05 -0400

    Template styles refactor

--- static/sidebar/js/sidebar.js
+++ static/sidebar/js/sidebar.js
@@ -3,7 +3,8 @@ for (let link of links) {
   let link_value = link.attributes['data-active'].value
   let exists = document.querySelector(`[id*="${link_value}-body-container"]`)
   if (exists !== null) {
-    link.classList.replace("text-gray-500", "text-gray-900")
-    link.classList.add("bg-blue-50")
+    link.classList.replace("text-gray-500", "text-gray-700")
+    link.classList.add("bg-teal-50")
+    link.firstElementChild.classList.add("text-teal-400")
   }
 }
