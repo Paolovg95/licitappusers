756c4f7cfa66d55a14a5de2f8b1bb3af45b934b2
paolovg95 <paolo9517@gmail.com>
2024-07-05 00:42:56 -0400

    Tab bar in licitaciones refactor and panel mockup cards added

--- static/sidebar/js/sidebar.js
+++ static/sidebar/js/sidebar.js
@@ -3,7 +3,7 @@ for (let link of links) {
   let link_value = link.attributes['data-active'].value
   let exists = document.querySelector(`[id*="${link_value}-body-container"]`)
   if (exists !== null) {
-    link.classList.replace("hover:bg-gray-800", "bg-gray-800")
+    link.classList.replace("hover:bg-gray-100","bg-emerald-50")
     link.firstElementChild.classList.add("text-emerald-400")
   }
 }
